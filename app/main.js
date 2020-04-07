3e78510a4d060af034390636ea43a9c2465401e0
tungdt <duongthanhtungvn01@gmail.com>
2020-04-07 19:30:41 +0700

    Add demo

--- app/main.js
+++ app/main.js
@@ -4,7 +4,7 @@ let fs = require('fs');
 
 http.createServer(function (req, res) {
     let q = url.parse(req.url, true);
-    let filename = "app/." + q.pathname;
+    let filename =  './' + q.pathname;
     fs.readFile(filename, function (err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
